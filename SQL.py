4750224a50737db9f80aa569faf0bc9e9ab87025
Tomerre1 <58183173+Tomerre1@users.noreply.github.com>
2019-12-07 12:51:01 +0200

    sql

--- SQL.py
+++ SQL.py
@@ -6,39 +6,32 @@ conn = sqlite3.connect('Server.db')
 
 c = conn.cursor()
 
-#c.execute("""CREATE TABLE Game (
- #           first text,
-  #          last text,
-   #         age integer
-    #        )""")
-def insert_player(p):
-    with conn:
-        c.execute("INSERT INTO Server VALUES (:first, :last, :age)", {'first': p.first, 'last': p.last, 'age': p.age})
+c.execute("""CREATE TABLE users (
+            first text,
+            last text,
+            age integer,
+            username text,
+            password text
+            )""")
+
+def login():
+    while True:
+        username=input("Enter UserName: ")
+        password=input("Enter Password: ")
+        find_user=("SELECT users WHERE username=? AND password=?")
+        c.execute=(find_user,[(username),(password)])
+        result=c.fetchall()
+        if result:
+            print("Welcome"+username)
+            break
+        else:
+            print("user and password not recognized")
+            enter_again=input("do you want enter again you login details y\n")
+            if enter_again.lower()==n:
+                exit(1)
 
 
-def get_emps_by_name(lastname):
-    c.execute("SELECT * FROM Server WHERE last=:last", {'last': lastname})
-    return c.fetchall()
 
 
-def update_age(p, age):
-    with conn:
-        c.execute("""UPDATE Server SET age = :age
-                    WHERE first = :first AND last = :last""",
-                  {'first': p.first, 'last': p.last, 'age': age})
-
-
-def remove_emp(p):
-    with conn:
-        c.execute("DELETE from Server WHERE first = :first AND last = :last",
-                  {'first': p.first, 'last': p.last})
-
-
-p1 = Player('ilia', 'elir', 23)
-p2 = Player('tal', 'itah', 23)
-find = get_emps_by_name('elir')
-print(find)
-insert_player(p1)
-insert_player(p2)
-print(c.fetchall())
+login()
 conn.close()
