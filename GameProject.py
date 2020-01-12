d9878facead7f78acc99be91b5b2d16f9261aa3f
Tomerre1 <58183173+Tomerre1@users.noreply.github.com>
2020-01-12 13:52:55 +0200

    דרישה 1.1 ודרישה 1.11
    פונקציית הרשמה ופונקציית כניסה דרך הDB

--- GameProject.py
+++ GameProject.py
@@ -101,3 +101,95 @@ def Menu():
             conn.close()
         else:
             print("\n Not Valid Choice Try again")
+
+def isUserType(UserType):
+    if UserType.upper()=='P' or UserType.upper()=='U' or UserType.upper()=='M':
+        return True
+    return False
+
+def isGender(gender):
+    if gender=='B' or gender=='G' or gender=='b' or gender=='g':
+        return True
+    return False
+
+def Proper_ID(id):
+    if len(id)==9:
+        return True
+    return False
+
+def Proper_First_Or_Last_Name(str):
+    if str.isalpha():
+        return True
+    return False
+
+def Proper_Age(age):
+    if age>0 and age<120:
+        return True
+    return False
+
+def Check_User_Type_From_DB(type,id):
+    if type=='M' or type=='m':
+        ManagerMenu(id)
+        return True
+    elif type=='P' or type=='p':
+       menu_parent(id)
+       return True
+    elif type=='U' or type=='u':
+        Menu_User(id)
+        return True
+    else:
+        return False
+
+
+def Registerion():
+        UserType=input("Enter user type for registration, Please Enter:U-User,P-Parent:  ")
+        while isUserType(UserType)!=True:
+                UserType=input("Invalid input, Enter user type for registration, Please Enter: U-User,P-Parent:  ")
+        first=input("Enter first name: ")
+        while Proper_First_Or_Last_Name(first)!=True:
+            first=input("Invalid input,Enter first name only letters: ")
+        last=input("Enter last name: ")
+        while Proper_First_Or_Last_Name(last)!=True:
+            last=input("Invalid input,Enter last name only letters: ")
+        gender=input("Enter your gender [Boy or Girl]: ")
+        while isGender(gender)!=True:
+                gender=input("Invalid input, Enter your gender [Boy or Girl]: ")
+        age=int(input("Enter your age: "))
+        while Proper_Age(age)!=True:
+                age=int(input("Invalid Enter your age: "))
+        id=input("Enter your personal ID: ")
+        while Proper_ID(id)!=True:
+             id=input("Enter your personal ID properly [9 digits]: ")
+        if UserType.upper()=='U':
+            id_parent=input("Enter parent ID: ")
+            while Proper_ID(id_parent)!=True:
+                 id=input("Enter your parent properly [9 digits]: ")
+        else:
+            id_parent='none'
+        username=input("Enter username: ")
+        password=input("Enter password: ")
+        insert_into_database_user(UserType.upper(),first,last,gender.upper(),age,id,id_parent,username,password)      
+        
+def login():
+    while True:
+        username=input("Enter UserName: ")
+        password=input("Enter Password: ")
+        with sqlite3.connect("Data.db") as db:
+            cursor=db.cursor()
+        find_user=("SELECT * FROM users WHERE username=? AND password=?")
+        cursor.execute(find_user,[(username),(password)])
+        result=cursor.fetchall()
+        
+        if result:
+            print("Welcome "+username)
+            type=result[0][0]
+            id=result[0][5]
+            Check_User_Type_From_DB(type,id)
+            return True
+        else:
+            print("user and password not recognized")
+            enter_again=input("do you want enter again you login details [y/n]: ")
+            if enter_again.lower()== 'n':
+                print("GoodBye")
+                return False      
+
